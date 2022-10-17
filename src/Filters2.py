37357eef4fa775a6d60563853c836083d2a351ad
Bruno Di Sanzo <bdisanzo@itba.edu.ar>
2022-10-17 13:00:44 -0300

    Usanso Signal probando

--- src/Filters2.py
+++ src/Filters2.py
@@ -13,136 +13,253 @@ class FilterClass:
         self.transferFunction = None    #transferencia final
         self.tfLP = None                #transferencia del pasabajos
 
+    '''
 
-class ButterworthApprox(FilterClass):
-    def __init__(self):
-        super().__init__()
+    '''
+    def getLPTransferFunction(self, Fp, Fa, Ap, Aa, btype):
+        Wp = Fp * 2 * np.pi
+        Wa = Fa * 2 * np.pi
 
-        def getLPTransferFunction(self, Fp, Fa, Ap, Aa):
-            Wp = Fp * 2 * np.pi()
-            Wa = Fa * 2 * np.pi()
+        a = [1]
+        b = [1]
+
+        if (btype == "butter"):
             N, Wn = ss.buttord(Wp, Wa, Ap, Aa, True)
             self.n = N
-            b, a = ss.butter(N, Wn, 'lowpass', True)
+            b, a = ss.butter(N, Wn, 'lowpass', analog = True)
 
-            return ss.TransferFunction(b,a)
+        elif (btype == "cheby1"):
+            N, Wn = ss.cheb1ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b, a = ss.cheby1(N, Ap, Wn, 'lowpass', analog = True)
 
-        def getHPTransferFunction(self, Fp, Fa, Ap, Aa):
-            Wp = 1
-            Wa = 2 * np.pi() * Fp / (Fa * 2 * np.pi())
+        elif (btype == "cheby2"):
+            N, Wn = ss.cheb2ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b, a = ss.cheby2(N, Aa, Wn, 'lowpass', analog = True)
 
+        elif (btype == "ellip"):
+            N, Wn = ss.ellipord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b, a = ss.ellip(N, Ap, Aa, Wn, 'lowpass', analog=True)
+
+        self.transferFunction = ss.TransferFunction(b,a)
+
+        return self.transferFunction
+
+    '''
+
+    '''
+    def getHPTransferFunction(self, Fp, Fa, Ap, Aa, btype):
+        Wp = 1
+        Wa = 2 * np.pi * Fp / (Fa * 2 * np.pi)
+
+        b1 = [1]
+        a1 = [1]
+
+        if (btype == "butter"):
             N, Wn = ss.buttord(Wp, Wa, Ap, Aa, True)
             self.n = N
-            b1, a1 = ss.butter(N, Wn, 'lowpass', True)
-            b, a = ss.lp2hp(b1,a1, Fp * 2 * np.pi())
+            b1, a1 = ss.butter(N, Wn, 'lowpass', analog=True)
 
-            return ss.TransferFunction(b, a)
+        elif (btype == "cheby1"):
+            N, Wn = ss.cheb1ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby1(N, Ap, Wn, 'lowpass', analog=True)
 
-        def getBPTransferFunction(self, Fo, dFp, dFa, Ap, Aa):
+        elif (btype == "cheby2"):
+            N, Wn = ss.cheb2ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby2(N, Aa, Wn, 'lowpass', analog=True)
 
-            Fa = dFa / 2 + np.sqrt((dFa**2 + 4 * Fo**2)) / 2        # Hallo el valor de la frecuencia de atenuación más alta
+        elif (btype == "ellip"):
+            N, Wn = ss.ellipord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.ellip(N, Ap, Aa, Wn, 'lowpass', analog=True)
 
-            Wp = 1
-            Wa = np.abs((-(2 * np.pi() * Fa) ** 2 + (Fo * 2 * np.pi()) ** 2) /
-                        (- (dFp * 2 *np.pi()) * (2 * np.pi() * Fa)))
 
+        b, a = ss.lp2hp(b1,a1, Fp * 2 * np.pi)
+
+        self.transferFunction = ss.TransferFunction(b, a)
+
+        return self.transferFunction
+
+    '''
+
+    '''
+    def getBPTransferFunctionBW(self, Fo, dFp, dFa, Ap, Aa, btype):
+
+        Fa = dFa / 2 + np.sqrt((dFa**2 + 4 * Fo**2)) / 2        # Hallo el valor de la frecuencia de atenuación más alta
+
+        Wp = 1
+        Wa = np.abs((-(2 * np.pi * Fa) ** 2 + (Fo * 2 * np.pi) ** 2) /
+                    (- (dFp * 2 *np.pi) * (2 * np.pi * Fa)))
+
+        b1 = [1]
+        a1 = [1]
+
+        if (btype == "butter"):
             N, Wn = ss.buttord(Wp, Wa, Ap, Aa, True)
             self.n = N
-            b1, a1 = ss.butter(N, Wn, 'lowpass', True)
-            b, a = ss.lp2bp(b1, a1, Fo * 2 * np.pi(), dFp * 2 *np.pi())
+            b1, a1 = ss.butter(N, Wn, 'lowpass', analog=True)
 
-            return ss.TransferFunction(b, a)
+        elif (btype == "cheby1"):
+            N, Wn = ss.cheb1ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby1(N, Ap, Wn, 'lowpass', analog=True)
 
-        def getBPTransferFunction(self, Fps, Fas, Ap, Aa):
+        elif (btype == "cheby2"):
+            N, Wn = ss.cheb2ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby2(N, Aa, Wn, 'lowpass', analog=True)
 
-            Fo = np.sqrt(Fps[0]*Fps[1])
-            Fp = Fps[1]
+        elif (btype == "ellip"):
+            N, Wn = ss.ellipord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.ellip(N, Ap, Aa, Wn, 'lowpass', analog=True)
 
-            dFp = Fps[1] - Fps[0]
+        b, a = ss.lp2bp(b1, a1, Fo * 2 * np.pi, dFp * 2 *np.pi)
 
-            if (Fas[1] / Fo < Fo / Fas[0]):
-                Fa = Fas[1]
+        self.transferFunction = ss.TransferFunction(b, a)
 
-            else:
-                Fa = 2*Fo - Fas[0]
+        return self.transferFunction
 
-            Wp = 1
-            Wa = np.abs((-(2 * np.pi() * Fa) ** 2 + (Fo * 2 * np.pi()) ** 2) /
-                        (- (dFp * 2 * np.pi()) * (2 * np.pi() * Fa)))
+    '''
+    
+    '''
+    def getBPTransferFunctionFreq(self, Fps, Fas, Ap, Aa, btype):
 
+        Fo = np.sqrt(Fps[0]*Fps[1])
+        Fp = Fps[1]
+
+        dFp = Fps[1] - Fps[0]
+
+        if (Fas[1] / Fo < Fo / Fas[0]):
+            Fa = Fas[1]
+
+        else:
+            Fa = 2*Fo - Fas[0]
+
+        Wp = 1
+        Wa = np.abs((-(2 * np.pi * Fa) ** 2 + (Fo * 2 * np.pi) ** 2) /
+                    (- (dFp * 2 * np.pi) * (2 * np.pi * Fa)))
+
+        b1 = [1]
+        a1 = [1]
+
+        if (btype == "butter"):
             N, Wn = ss.buttord(Wp, Wa, Ap, Aa, True)
             self.n = N
-            b1, a1 = ss.butter(N, Wn, 'lowpass', True)
-            b, a = ss.lp2bp(b1, a1, Fo * 2 * np.pi(), dFp * 2 *np.pi())
+            b1, a1 = ss.butter(N, Wn, 'lowpass', analog=True)
 
-            return ss.TransferFunction(b, a)
+        elif (btype == "cheby1"):
+            N, Wn = ss.cheb1ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby1(N, Ap, Wn, 'lowpass', analog=True)
 
-        def getBSTransferFunction(self, Fo, dFp, dFa, Ap, Aa):
+        elif (btype == "cheby2"):
+            N, Wn = ss.cheb2ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby2(N, Aa, Wn, 'lowpass', analog=True)
 
-            Fa = dFa / 2 + np.sqrt((dFa ** 2 + 4 * Fo ** 2)) / 2  # Hallo el valor de la frecuencia de atenuación más alta
+        elif (btype == "ellip"):
+            N, Wn = ss.ellipord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.ellip(N, Ap, Aa, Wn, 'lowpass', analog=True)
 
-            Wp = 1
-            Wa = np.abs((-(2 * np.pi() * Fa) ** 2 + (Fo * 2 * np.pi()) ** 2) /
-                        (- (dFp * 2 * np.pi()) * (2 * np.pi() * Fa)))
 
+        b, a = ss.lp2bp(b1, a1, Fo * 2 * np.pi, dFp * 2 *np.pi)
+
+        self.transferFunction = ss.TransferFunction(b, a)
+
+        return self.transferFunction
+
+    '''
+    
+    '''
+    def getBSTransferFunctionBW(self, Fo, dFp, dFa, Ap, Aa, btype):
+
+        Fa = dFa / 2 + np.sqrt((dFa ** 2 + 4 * Fo ** 2)) / 2  # Hallo el valor de la frecuencia de atenuación más alta
+
+        Wp = 1
+        Wa = np.abs(((dFp * 2 * np.pi) * (2 * np.pi * Fa)) /
+            (-(2 * np.pi * Fa) ** 2 + (Fo * 2 * np.pi) ** 2))
+
+        b1 = [1]
+        a1 = [1]
+
+        if (btype == "butter"):
             N, Wn = ss.buttord(Wp, Wa, Ap, Aa, True)
             self.n = N
-            b1, a1 = ss.butter(N, Wn, 'lowpass', True)
-            b, a = ss.lp2hp(b1, a1, Fo * 2 * np.pi())
+            b1, a1 = ss.butter(N, Wn, 'lowpass', analog=True)
 
-            return ss.TransferFunction(b, a, Fo * 2 * np.pi())
+        elif (btype == "cheby1"):
+            N, Wn = ss.cheb1ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby1(N, Ap, Wn, 'lowpass', analog=True)
 
-        def getBSTransferFunction(self, Fps, Fas, Ap, Aa):
+        elif (btype == "cheby2"):
+            N, Wn = ss.cheb2ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby2(N, Aa, Wn, 'lowpass', analog=True)
 
-            Fo = np.sqrt(Fps[0] * Fps[1])
-            Fp = Fps[1]
+        elif (btype == "ellip"):
+            N, Wn = ss.ellipord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.ellip(N, Ap, Aa, Wn, 'lowpass', analog=True)
 
-            dFp = Fps[1] - Fps[0]
+        b, a = ss.lp2bs(b1, a1, Fo * 2 * np.pi, dFp * 2 * np.pi)
 
-            if (Fas[1] / Fo < Fo / Fas[0]):
-                Fa = Fas[1]
+        self.transferFunction = ss.TransferFunction(b, a)
 
-            else:
-                Fa = 2 * Fo - Fas[0]
+        return self.transferFunction
 
-            Wp = 1
-            Wa = np.abs((-(2 * np.pi() * Fa) ** 2 + (Fo * 2 * np.pi()) ** 2) /
-                        (- (dFp * 2 * np.pi()) * (2 * np.pi() * Fa)))
+    '''
 
+    '''
+    def getBSTransferFunctionFreq(self, Fps, Fas, Ap, Aa, btype):
+
+        Fo = np.sqrt(Fps[0] * Fps[1])
+        Fp = Fps[1]
+
+        dFp = Fps[1] - Fps[0]
+
+        if (Fas[1] / Fo < Fo / Fas[0]):
+            Fa = Fas[1]
+
+        else:
+            Fa = 2 * Fo - Fas[0]
+
+        Wp = 1
+        Wa = np.abs(((dFp * 2 * np.pi) * (2 * np.pi * Fa)) /
+                    (-(2 * np.pi * Fa) ** 2 + (Fo * 2 * np.pi) ** 2))
+
+        b1 = [1]
+        a1 = [1]
+
+        if (btype == "butter"):
             N, Wn = ss.buttord(Wp, Wa, Ap, Aa, True)
             self.n = N
-            b1, a1 = ss.butter(N, Wn, 'lowpass', True)
-            b, a = ss.lp2hp(b1, a1, Fo * 2 * np.pi())
+            b1, a1 = ss.butter(N, Wn, 'lowpass', analog=True)
 
-            return ss.TransferFunction(b, a, Fp * 2 * np.pi())
+        elif (btype == "cheby1"):
+            N, Wn = ss.cheb1ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby1(N, Ap, Wn, 'lowpass', analog=True)
 
+        elif (btype == "cheby2"):
+            N, Wn = ss.cheb2ord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.cheby2(N, Aa, Wn, 'lowpass', analog=True)
 
-class ChebyshevApproxI(FilterClass):
-    def __init__(self, Ap, Aa, Fp, Fa):
-        super().__init__(Ap, Aa)
+        elif (btype == "ellip"):
+            N, Wn = ss.ellipord(Wp, Wa, Ap, Aa, True)
+            self.n = N
+            b1, a1 = ss.ellip(N, Ap, Aa, Wn, 'lowpass', analog=True)
 
-        self.Fa = Fa
-        self.Fp = Fp
-        self.Wp = 1
-        self.Wa = Fa / Fp
+        b, a = ss.lp2bs(b1, a1, Fo * 2 * np.pi, dFp * 2 * np.pi)
 
-        N, Wn = ss.cheb1ord(self.Wp, self.Wa, Ap, Aa, True)
-        self.tfLP = ss.cheby1(N, Ap, Wn, 'lowpass', True)
+        self.transferFunction = ss.TransferFunction(b, a)
 
-        wp, m, p = ss.bode(self.tfLP, np.linspace(0, 2, 100))
-
-        plt.plot(wp, 10 ** (m / 20))
-        plt.show()
-
-class ChebyshevApproxII(FilterClass):
-    def __init__(self, Ap, Aa, Fp, Fa):
-        super().__init__(Ap, Aa)
-
-        self.Fa = Fa
-        self.Fp = Fp
-        self.Wp = 1
-        self.Wa = Fa / Fp
-
-        N, Wn = ss.cheb2ord(self.Wp, self.Wa, Ap, Aa, True)
-        self.tfLP = ss.cheby2(N, Wn, 'lowpass', True)
+        return self.transferFunction
 
--- src/MyWindow.py
+++ src/MyWindow.py
@@ -3,8 +3,10 @@ from PyQt5.QtWidgets import QMainWindow
 
 # Project modules
 from src.ui.mainwindow import Ui_MainWindow
-from src.Filters2 import ButterworthApprox,ChebyshevApproxI, ChebyshevApproxII
-import src.Filters
+from src.Filters2 import FilterClass
+import scipy.signal as ss
+import matplotlib.pyplot as plt
+import numpy as np
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -12,8 +14,39 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        tipo = "HP"
+        Fp = 15
+        Fa = 1
+        Ap = 5
+        Aa = 200
+        f = FilterClass()
+        tF = f.getHPTransferFunction(Fp, Fa, Ap, Aa, "ellip")
 
-        #f1 = src.Filters.ChebyshevApproxI(1,20,1,1.1)
-        f = ButterworthApprox(1, 20, 1, 1.1)
+
+        if tipo == "LP":
+            w = np.logspace(np.log10(Fp *2*np.pi / 10), np.log10(Fa *2*np.pi* 10), 1000)
+            w, m, p = ss.bode(tF, w, n=1000)
+            plt.semilogx(w / (2 * np.pi), m)
+
+            plt.fill([Fp/10, Fp, Fp, Fp/10], [0, 0, -Ap, -Ap], '0.9', lw=0)
+            plt.fill([Fa, Fa*10, Fa*10, Fa], [-Aa, -Aa, -Aa -10, -Aa -10], '0.9', lw=0)
+
+        elif tipo == "HP":
+            w = np.logspace(np.log10(Fa * 2 * np.pi / 10), np.log10(Fp * 2 * np.pi * 10), 1000)
+            w, m, p = ss.bode(tF, w, n=1000)
+            plt.semilogx(w / (2 * np.pi), m)
+
+            plt.fill([Fp, Fp*10, Fp*10, Fp], [0, 0, -Ap, -Ap], '0.9', lw=0)
+            plt.fill([Fa/10, Fa, Fa, Fa/10], [-Aa, -Aa, -Aa*2, -Aa*2], '0.9', lw=0)
+
+        plt.show()
+
+
+
+
+
+
+
+
 
 
